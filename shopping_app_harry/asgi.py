f084b39e96b7e820e2e9fc457042c8ec0f0d408f
Vivek Kumar <32337899+vickytilotia@users.noreply.github.com>
2021-02-16 13:30:22 +0530

    first commit

---
+++ shopping_app_harry/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for shopping_app_harry project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopping_app_harry.settings')
+
+application = get_asgi_application()
